ec2f96e20c7bd61de2e0fb420b6a03a401c1770d
jpaltahona <85860736+jerssonJ@users.noreply.github.com>
2023-10-31 14:09:17 -0500

    Update Card.js

--- openweather/src/components/Card.js
+++ openweather/src/components/Card.js
@@ -49,8 +49,10 @@ const Card = ({loadingData, showData, weather, forecast}) => {
                                 <div className="col-md-4">
                                     <h3 className="card-title">{weather.name}</h3>
                                     <p className="card-date">{date}</p>
-                                    <h1 className="card-temp">{(weather.main.temp - 273.15).toFixed(1)}ºC</h1>
-                                    <p className="card-desc"><img src={iconUrl} alt="icon"/>{weather.weather[0].description}</p>
+                                 
+                                        <h1 className="card-temp">{(weather.main.temp - 273.15).toFixed(1)}ºC</h1>
+                                        <p className="card-desc"><img src={iconUrl} alt="icon"/>{weather.weather[0].description}</p>
+                                    
                                     <img src="https://images.pexels.com/photos/1816714/pexels-photo-1816714.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" className="img-fluid rounded-start" alt="..."/>
                                 </div>
                                 <div className="col-md-8">
